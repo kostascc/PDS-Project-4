b5e50f1fe73b82d264e14fc9456ee2828dfe5d68
Kostas <kachatzis@ece.auth.gr>
2021-10-04 01:15:31 +0300

    Adopt MPI utilities

---
+++ include/MPIUtil.hpp
@@ -0,0 +1,240 @@
+/**
+ * Parallel & Distributed Systems
+ * Project 4
+ * 
+ * Ⓒ 2021 K. Chatzis
+ * kachatzis <at> ece.auth.gr
+ **/
+
+#ifndef MPI_UTIL_HPP__
+#define MPI_UTIL_HPP__
+
+
+#include "mpi.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+/**********************************************
+ *            Debugging Options
+ **********************************************/
+#define DBG_MPI_INIT true
+#define DBG_MPI_COMM true
+/**********************************************/
+
+#define MPI_MASTER_NODE_IDX 0
+
+using namespace std;
+
+class MPIUtil {
+
+    private:
+
+        int nodeIdx;
+        int clusterSize;
+        int prevNode;
+        int nextNode;
+
+
+    public:
+
+        /**
+         * Initializes MPI Communications
+         **/
+        MPIUtil();
+
+        /*********************************************
+         *                 Get/Set
+         *********************************************/
+
+        /**
+         * Current node Index
+         **/
+        int NodeIdx(){return nodeIdx;}
+
+        /**
+         * Population of nodes in the cluster
+         **/
+        int ClusterSize(){return clusterSize;}
+
+        /**
+         * Index of next node (Round allocation)
+         **/
+        int NextNode(){return nextNode;}
+
+        /**
+         * Index of previous node (Round allocation)
+         **/
+        int PrevNode(){return prevNode;}
+
+        /**
+         * Returns true if the current node is the master
+         **/
+        bool IsMaster(){return nodeIdx==MPI_MASTER_NODE_IDX;};
+
+        /*********************************************
+         *            Program Utilities
+         *********************************************/
+
+        /**
+         * Aborts MPI Execution in COMM_WORLD.
+         * (The Whole job stops)
+         **/
+        void Abort();
+
+        /**
+         * Aborts MPI Execution in COMM_WORLD,
+         * with an error message.
+         * (The Whole job stops)
+         * 
+         * @param msg 
+         */
+        void AbortMsg(char* msg);
+
+        /**
+         * Finishes local job silently
+         */
+        void FinishLocal();
+
+        /**
+         * Finishes local job silently
+         */
+        void Finalize();
+
+
+        /*********************************************
+         *                MPI Calls
+         *********************************************/
+
+        /**
+         * Usage:
+         * 
+         * For non blocking:
+         *   Send_nb(MPI_MODE_DISTRIBUTING, X, n, node_send, mpi_request);
+         *   SendWait(mpi_request); // call later
+         * 
+         * For blocking:
+         *   Send_b(...)
+         * 
+         * Use wait at any time to block while receiving or sending:
+         *   SendWait(mpi_request); // Wait for sending to finish
+         * 
+         **/
+
+        /**
+         * Non-Blocking Send with DataType.
+         * 
+         * @param tag 
+         * @param buffer 
+         * @param length 
+         * @param partner 
+         * @param type 
+         * @param request 
+         */
+        void Send_t(int tag, void* buffer, int length, int partner, MPI_Datatype type, MPI_Request request[]);
+
+        /**
+         * Non-Blocking Receive with DataType.
+         * 
+         * @param tag 
+         * @param buffer 
+         * @param length 
+         * @param partner 
+         * @param type 
+         * @param request 
+         */
+        void Receive_t(int tag, void* buffer, int length, int partner, MPI_Datatype type, MPI_Request request[]);
+
+        /**
+         * Non-Blocking Send,
+         * with default DataType: integer.
+         * 
+         * @param tag 
+         * @param buffer 
+         * @param length 
+         * @param partner 
+         * @param request 
+         */
+        void Send(int tag, int* buffer, int length, int partner, MPI_Request request[]);
+
+        /**
+         * Non-Blocking Receive,
+         * with default DataType: integer.
+         * 
+         * @param tag 
+         * @param buffer 
+         * @param length 
+         * @param partner 
+         * @param request 
+         */
+        void Receive(int tag, int* buffer, int length, int partner, MPI_Request request[]);
+
+        /**
+         * Blocks until send has finished.
+         * 
+         * @param request 
+         */
+        void SendWait(MPI_Request request[]);
+
+
+        /**
+         * Blocks until receive has finished.
+         * 
+         * @param request 
+         */
+        void ReceiveWait(MPI_Request request[]);
+
+        // /**
+        //  * Blocking Send,
+        //  * with default DataType: Double.
+        //  * 
+        //  * @param code 
+        //  * @param Y 
+        //  * @param m 
+        //  * @param partner 
+        //  * @param request 
+        //  */
+        // void Send_b(int code, double* Y, int m, int partner, MPI_Request request[]);
+
+
+        // /**
+        //  * Blocking Receive,
+        //  * with default DataType: Double.
+        //  * 
+        //  * @param code 
+        //  * @param Z 
+        //  * @param m 
+        //  * @param partner 
+        //  * @param request 
+        //  */
+        // void Receive_b(int code, double* Z, int m, int partner, MPI_Request request[]);
+
+        // /**
+        //  * Blocking Send with DataType.
+        //  * 
+        //  * @param code 
+        //  * @param Y 
+        //  * @param m 
+        //  * @param partner 
+        //  * @param type 
+        //  * @param request 
+        //  */
+        // void Send_b_t(int code, double* Y, int m, int partner, MPI_Datatype type, MPI_Request request[]);
+
+        // /**
+        //  * Blocking Receive with DataType.
+        //  * 
+        //  * @param code 
+        //  * @param Z 
+        //  * @param m 
+        //  * @param partner 
+        //  * @param type 
+        //  * @param request 
+        //  */
+        // void Receive_b_t(int code, double* Z, int m, int partner, MPI_Datatype type, MPI_Request request[]);
+
+};
+
+#endif
---
+++ src/MPIUtil.cpp
@@ -0,0 +1,183 @@
+/**
+ * Parallel & Distributed Systems
+ * Project 4
+ * 
+ * 2021 Ⓒ K. Chatzis
+ * kachatzis <at> ece.auth.gr
+ **/
+
+#include "MPIUtil.hpp"
+
+
+MPIUtil::MPIUtil(){
+    // Initiate MPI
+    MPI_Init(NULL, NULL);
+
+    // Get Cluster Size
+    MPI_Comm_size(MPI_COMM_WORLD, &clusterSize);
+
+    // Get Node ID
+    MPI_Comm_rank(MPI_COMM_WORLD, &nodeIdx);
+
+    nextNode =  nodeIdx >= clusterSize ? 
+                0 : nodeIdx + 1;
+
+    prevNode =  nodeIdx == 0 ? 
+                clusterSize - 1 : nodeIdx - 1;
+    
+    #if DBG_MPI_INIT == true
+        printf("[Info] MPI node:%d, cluster: %d, master: %d, prev: %d, next: %d\n", 
+            nodeIdx, clusterSize, MPI_MASTER_NODE_IDX, prevNode, nextNode);
+    #endif
+}
+
+
+void MPIUtil::Send_t(int tag, void* buffer, int length, int partner, MPI_Datatype type, MPI_Request request[])
+{
+
+    #if DBG_MPI_COMM == true
+        printf("[Info] MPI Node %d Sending to %d (length: %d, tag: %d)\n", nodeIdx, partner, length, tag);
+    #endif
+
+    MPI_Isend(
+        buffer,         // *Buffer
+        length,          // Count
+        type,     // Type
+        partner,   // Destination
+        tag,    // Tag
+        MPI_COMM_WORLD,   // Comm
+        &(request[1])   // *Request
+    );
+    
+}
+
+
+void MPIUtil::Receive_t(int tag, void* buffer, int length, int partner, MPI_Datatype type, MPI_Request request[])
+{
+
+    #if DBG_MPI_COMM == true
+        printf("[Info] MPI Node %d receiving from %d (length: %d, tag: %d)\n", nodeIdx, partner, length, tag);
+    #endif
+
+    MPI_Irecv(
+        buffer,         // *Buffer
+        length,          // Count
+        type,     // Type
+        partner,   // Destination
+        tag,    // Tag
+        MPI_COMM_WORLD,   // Comm
+        &(request[0])   // *Request
+    );
+
+}
+
+
+void MPIUtil::Send(int tag, int* buffer, int length, int partner, MPI_Request request[])
+{
+
+    Send_t(tag, buffer, length, partner, MPI_INT, request);
+    
+}
+
+
+void MPIUtil::Receive(int tag, int* buffer, int length, int partner, MPI_Request request[])
+{
+
+    Receive_t(tag, buffer, length, partner, MPI_INT, request);
+
+}
+
+
+void MPIUtil::SendWait(MPI_Request request[])
+{
+
+    MPI_Status status;
+    MPI_Wait(&(request[1]), &status);
+    
+}
+
+
+void MPIUtil::ReceiveWait(MPI_Request request[])
+{
+
+    MPI_Status status;
+    MPI_Wait(&(request[0]), &status);
+
+}
+
+
+void MPIUtil::Abort()
+{
+    // #if DBG_MPI_COMM == true
+        printf("[Error] MPI Abort\n");
+    // #endif
+
+    MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+}
+
+
+void MPIUtil::AbortMsg(char* msg)
+{
+    // #if DBG_MPI_COMM == true
+        printf("[Error] MPI Abort (%s)\n", msg);
+    // #endif
+
+    MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+}
+
+
+void MPIUtil::FinishLocal()
+{
+    #if DBG_MPI_INIT == true
+        printf("[Info] MPI node %d finished\n", nodeIdx);
+    #endif
+
+    MPI_Finalize();
+    exit(EXIT_SUCCESS);
+}
+
+
+void MPIUtil::Finalize()
+{
+    #if DBG_MPI_INIT == true
+        printf("[Info] MPI node %d finalized\n", nodeIdx);
+    #endif
+
+    MPI_Finalize();
+}
+
+
+// void MPIUtil::Send_b(int tag, void* buffer, int length, int partner, MPI_Request request[])
+// {
+
+//     Send_nb(tag, buffer, length, partner, request);
+//     SendWait(request);
+    
+// }
+
+
+// void MPIUtil::Receive_b(int tag, void* buffer, int length, int partner, MPI_Request request[])
+// {
+
+//     Receive_nb(tag, buffer, length, partner, request);
+//     ReceiveWait(request);
+
+// }
+
+
+// void MPIUtil::Send_b_t(int tag, void* buffer, int length, int partner, MPI_Datatype type, MPI_Request request[])
+// {
+
+//     Send_nb_t(tag, buffer, length, partner, type, request);
+//     SendWait(request);
+    
+// }
+
+
+// void MPIUtil::Receive_b_t(int tag, void* buffer, int length, int partner, MPI_Datatype type, MPI_Request request[])
+// {
+
+//     Receive_nb_t(tag, buffer, length, partner, type, request);
+//     ReceiveWait(request);
+
+// }
